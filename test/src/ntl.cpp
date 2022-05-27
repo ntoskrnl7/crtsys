4ab7471ae9f76680c0f38527bc4d5fabc892a2f5
J.K Lee <47030112+ntoskrnl7@users.noreply.github.com>
2022-05-28 02:47:05 +0900

    Update ntl.cpp

--- test/src/ntl.cpp
+++ test/src/ntl.cpp
@@ -1,3 +1,27 @@
+#include <ntdef.h>
+typedef PSTRING PUTF8_STRING;
+
+#ifndef DECLSPEC_RESTRICT
+#if (_MSC_VER >= 1915) && !defined(MIDL_PASS)
+#define DECLSPEC_RESTRICT   __declspec(restrict)
+#else
+#define DECLSPEC_RESTRICT
+#endif
+#endif
+
+#ifndef NTDDI_WIN10_FE
+#define NTDDI_WIN10_FE                      0x0A00000A
+#endif
+#ifndef NTDDI_WIN10_FE
+#define NTDDI_WIN10_CO                      0x0A00000B
+#endif
+#ifndef ENCLAVE_SHORT_ID_LENGTH
+#define ENCLAVE_SHORT_ID_LENGTH             16
+#endif
+#ifndef ENCLAVE_LONG_ID_LENGTH
+#define ENCLAVE_LONG_ID_LENGTH              32
+#endif
+
 #include <ntl/expand_stack>
 
 #include <string>
