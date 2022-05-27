cb227db9df9d3d66e9b399240cd87b45cc229621
J.K Lee <47030112+ntoskrnl7@users.noreply.github.com>
2022-05-28 02:38:18 +0900

    Update main.cpp

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,5 +1,29 @@
-#include <wdm.h>
+#include <ntdef.h>
+typedef PSTRING PUTF8_STRING;
+
+#ifndef DECLSPEC_RESTRICT
+#if (_MSC_VER >= 1915) && !defined(MIDL_PASS)
+#define DECLSPEC_RESTRICT   __declspec(restrict)
+#else
+#define DECLSPEC_RESTRICT
+#endif
+#endif
+
+#ifndef NTDDI_WIN10_FE
+#define NTDDI_WIN10_FE                      0x0A00000A
+#endif
+#ifndef NTDDI_WIN10_FE
+#define NTDDI_WIN10_CO                      0x0A00000B
+#endif
+#ifndef ENCLAVE_SHORT_ID_LENGTH
+#define ENCLAVE_SHORT_ID_LENGTH             16
+#endif
+#ifndef ENCLAVE_LONG_ID_LENGTH
+#define ENCLAVE_LONG_ID_LENGTH              32
+#endif
+
 #include <stdio.h>
+#include <wdm.h>
 
 EXTERN_C DRIVER_INITIALIZE DriverEntry;
 EXTERN_C DRIVER_UNLOAD DriverUnload;
@@ -13,7 +37,7 @@ void test_all();
 
 #include <gtest/gtest.h>
 
-#if 0 // CRTSYS_USE_NTL_MAIN
+#if CRTSYS_USE_NTL_MAIN
 #include <iostream>
 #include <ntl/driver>
 
